07dca81640f8496ff1b03182e54f137591fc45d8
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-04-11 16:17:27 -0300

    add colors to variables

--- styles/variables.ts
+++ styles/variables.ts
@@ -3,7 +3,14 @@ import { createGlobalStyle } from "styled-components";
 const Variables = createGlobalStyle`
   :root {
 
-  
+  --white: #FFFFFF;
+
+  --very-dark-blue: #0D192C;
+  --dark-blue: #15263F;
+  --blue: #2E405A;
+  --cyan: #00FFF8;
+  --soft-blue: #8BACD9;
+  --black: #000000;
 
  
   
