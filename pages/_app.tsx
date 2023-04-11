8b91ceb62198791a3172b08f5ae011e1878d0942
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-04-11 16:17:49 -0300

    add GlobalStyle and Variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import GlobalStyle from "@/styles/global";
+import Variables from "@/styles/variables";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Variables />
+      <Component {...pageProps} />;
+    </>
+  );
 }
